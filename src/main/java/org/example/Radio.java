7b15325680a1f68965832a4ed828a74536a15b5b
Mikhail Valerko <mixelv@mail.ru>
2023-02-22 15:24:05 +0100

    Добавлена функциоанльность управления звуком. Расиширены автотесты, добалвены комментарии к коду

--- src/main/java/org/example/Radio.java
+++ src/main/java/org/example/Radio.java
@@ -1,13 +1,18 @@
 package org.example;
 
 public class Radio {
+
+    //Вносим поля в которых будет храниться информация о радиостанции и об уровне громкости звука
     private int numberCurrentStation;
     private int soundVolume;
+
+    //Реализуем метод получения информации о номере текущей радиостанции
 
     public int getNumberCurrentStation() {
         return numberCurrentStation;
     }
 
+    //Реализуем метод прямой установки номера текущей радиостанции (Всего эфир состоит из 10 станций)
     public void setNumberCurrentStation(int newNumberCurrentStation) {
         if (newNumberCurrentStation < 0) {
             return;
@@ -18,7 +23,9 @@ public class Radio {
         numberCurrentStation = newNumberCurrentStation;
     }
 
-    public void next () {
+    //Реализуем метод переключения текущей радиостанции на следующую по счету
+
+    public void next() {
         if (numberCurrentStation == 9) {
             numberCurrentStation = 0;
             return;
@@ -26,7 +33,8 @@ public class Radio {
         numberCurrentStation++;
 
     }
-    public void prev () {
+    //Реализуем метод переключения текущей радиостанции на предыдущую по счету
+    public void prev() {
         if (numberCurrentStation == 0) {
             numberCurrentStation = 9;
             return;
@@ -34,5 +42,38 @@ public class Radio {
         numberCurrentStation--;
     }
 
+    //Реализуем метод получения информации об уровне звука
+    public int getSoundVolume() {
+        return soundVolume;
+    }
+
+    /*public void setSoundVolume(int newSoundVolume) {
+        if (newSoundVolume < 0) {
+            return;
+        }
+        if (newSoundVolume > 10) {
+            return;
+        }
+        numberCurrentStation = newSoundVolume;
+    }
+     */
+
+    //Реализуем метод увеличения уровня звука на 1 единицу
+    public void volumePlus() {
+        if (soundVolume == 10) {
+            //soundVolume = 10;
+            return;
+        }
+        soundVolume++;
+    }
+    //Реализуем метод уменьшения уровня звука на 1 единицу
+    public void volumeMinus() {
+        if (soundVolume == 0) {
+            //soundVolume = 0;
+            return;
+        }
+        soundVolume--;
+
+    }
 }
 
--- src/test/java/RadioTest.java
+++ src/test/java/RadioTest.java
@@ -2,17 +2,19 @@ import org.example.Radio;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 public class RadioTest {
+
+    //Проверяем, что можем получить информацию о текущей радиостанции
     @Test
     public void getNumberCurrentStationTest () {
         Radio receiver = new Radio();
-
-        receiver.getNumberCurrentStation();
-
+        
         int expected = 0;
         int actual = receiver.getNumberCurrentStation();
 
         Assertions.assertEquals(expected, actual);
     }
+
+    //Проверяем, что напрямую невозможно установить стануию за верхними пределами эфира
     @Test
     public void setNumberCurrentStationTest () {
         Radio receiver = new Radio();
@@ -24,6 +26,7 @@ public class RadioTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    //Проверяем, что напрямую невозможно установить стануию за нижними пределами эфира
     @Test
     public void setNegativeNumberCurrentStationTest () {
         Radio receiver = new Radio();
@@ -35,6 +38,7 @@ public class RadioTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    //Проверяем, что напрямую возможно установить стануию в пределах валдидных значений эфира
     @Test
     public void setValidNumberCurrentStationTest () {
         Radio receiver = new Radio();
@@ -46,6 +50,7 @@ public class RadioTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    //Проверяем, что напрямую возможно установить стануию в пределах верхних граничных значений эфира
     @Test
     public void setEdgeNumberCurrentStationTest () {
         Radio receiver = new Radio();
@@ -57,6 +62,8 @@ public class RadioTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    //Проверяем, что напрямую возможно установить стануию в пределах верхних граничных значений эфира
     @Test
     public void setPreEdgeNumberCurrentStationTest () {
         Radio receiver = new Radio();
@@ -68,7 +75,7 @@ public class RadioTest {
 
         Assertions.assertEquals(expected, actual);
     }
-
+    //Проверяем, что напрямую возможно установить стануию в пределах нижних граничных значений эфира
     @Test
     public void setZeroNumberCurrentStationTest () {
         Radio receiver = new Radio();
@@ -80,6 +87,7 @@ public class RadioTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    //Проверяем, что напрямую возможно установить стануию в пределах нижних граничных значений эфира
     @Test
     public void setPreZeroNumberCurrentStationTest () {
         Radio receiver = new Radio();
@@ -91,6 +99,8 @@ public class RadioTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    //Проверяем, что механика "кругового" переключения по радиостанциям работает
     @Test
     public void nextTestNine () {
         Radio receiver = new Radio();
@@ -103,6 +113,7 @@ public class RadioTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    //Проверяем, что механика переключения по радиостанциям работает
     @Test
     public void nextTestValid () {
         Radio receiver = new Radio();
@@ -116,6 +127,32 @@ public class RadioTest {
         Assertions.assertEquals(expected, actual);
     }
 
+    //Проверяем, что механика переключения по радиостанциям работает
+    @Test
+    public void nextComboTestValid () {
+        Radio receiver = new Radio();
+        for (int i = 0; i < 4; i++) {
+            receiver.next();
+        }
+
+        int expected = 4;
+        int actual = receiver.getNumberCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    //Проверяем, что механика переключения по радиостанциям работает
+    @Test
+    public void nextTestValidFirst () {
+        Radio receiver = new Radio();
+
+        receiver.next();
+
+        int expected = 1;
+        int actual = receiver.getNumberCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    //Проверяем, что механика "кругового" переключения по радиостанциям работает
     @Test
     public void prevTestZero () {
         Radio receiver = new Radio();
@@ -127,6 +164,7 @@ public class RadioTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    //Проверяем, что механика переключения по радиостанциям работает
     @Test
     public void prevTestValid () {
         Radio receiver = new Radio();
@@ -136,6 +174,100 @@ public class RadioTest {
 
         int expected = 6;
         int actual = receiver.getNumberCurrentStation();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    //Проверяем, что метод получения информации о текущем уровне громкости работает
+    @Test
+    public void getSoundVolumeTest () {
+        Radio receiver = new Radio();
+
+        int expected = 0;
+        int actual = receiver.getSoundVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    //Проверяем, что механика переключения уровня громкости работает
+    @Test
+    public void plusSoundVolumeTest () {
+        Radio receiver = new Radio();
+
+        receiver.volumePlus();
+
+        int expected = 1;
+        int actual = receiver.getSoundVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    //Проверяем, что механика переключения уровня громкости работает
+    @Test
+    public void plusSoundVolumeValidTest () {
+        Radio receiver = new Radio();
+
+        for (int i = 0; i < 5; i++) {
+            receiver.volumePlus();
+        }
+
+        int expected = 5;
+        int actual = receiver.getSoundVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    //Проверяем, что механика переключения уровня громкости работает в рамках граничных значений
+    @Test
+    public void plusSoundVolumeLimitTest () {
+        Radio receiver = new Radio();
+
+        for (int i = 0; i < 10; i++) {
+            receiver.volumePlus();
+        }
+
+        int expected = 10;
+        int actual = receiver.getSoundVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    //Проверяем, что механика переключения уровня громкости работает в рамках граничных значений
+    @Test
+    public void plusSoundVolumeOverLimitTest () {
+        Radio receiver = new Radio();
+
+        for (int i = 0; i < 11; i++) {
+            receiver.volumePlus();
+        }
+
+        int expected = 10;
+        int actual = receiver.getSoundVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    //Проверяем, что механика переключения уровня громкости работает в рамках граничных значений
+    @Test
+    public void minusSoundVolumeTest () {
+        Radio receiver = new Radio();
+
+        receiver.volumeMinus();
+
+        int expected = 0;
+        int actual = receiver.getSoundVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+    //Проверяем, что механика переключения уровня громкости работает в рамках граничных значений
+    @Test
+    public void minusSoundVolumeValidTest () {
+        Radio receiver = new Radio();
+
+        for (int i = 0; i < 3; i++) {
+            receiver.volumePlus();
+        }
+        for (int i = 0; i < 2; i++) {
+            receiver.volumeMinus();
+        }
+
+        int expected = 1;
+        int actual = receiver.getSoundVolume();
 
         Assertions.assertEquals(expected, actual);
     }
